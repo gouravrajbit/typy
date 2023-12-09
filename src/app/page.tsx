5106d7bee46423b725374469bc510c4c3edd57b6
gouravrajbit <gouravr244@gmail.com>
2023-12-10 00:31:23 +0530

    Add some jsx rules

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,15 +4,10 @@ export default function Home() {
   return (
     <>
       <header>
-        <h1 className="font-mono text-lg font-bold text-center">
-          typy_/
-        </h1>
+        <h1 className="font-mono text-lg font-bold text-center">typy_/</h1>
       </header>
-      <main>
-
-      </main>
-      <footer>
-      </footer>
+      <main></main>
+      <footer></footer>
     </>
   );
 }
