86a22ada1385018fcdef7ea839dd664172bd0128
gouravrajbit <gouravr244@gmail.com>
2023-12-10 14:16:33 +0530

    Add input box

--- src/app/components/TypeBox.tsx
+++ src/app/components/TypeBox.tsx
@@ -1,13 +1,42 @@
+'use client';
 import { generate, count } from 'random-words';
+import { ChangeEvent, useCallback, useEffect, useMemo, useState } from 'react';
 
 export default function TypeBox() {
-  const words: string[] = generate({ min: 20, max: 30 });
+  const generateSentence = () => {
+    const words: string[] = generate({ min: 20, max: 30 });
+    const sentence: string = words.join(' ');
+    return sentence;
+  };
 
-  const sentence: string = words.join(' ');
+  const [sentence, setSentence] = useState<string>('');
+  const [written, setWritten] = useState<string>('');
+
+  useEffect(() => {
+    setSentence(generateSentence());
+  }, []);
+
+  const inputHandler = (event: ChangeEvent<HTMLInputElement>) => {
+    const value = event.target.value;
+    console.log(value);
+    if (value.length === sentence.length) {
+      setSentence(generateSentence());
+      setWritten('');
+    } else {
+      setWritten(event.target.value);
+    }
+  };
 
   return (
     <div className="mt-[30vh] font-mono text-2xl px-20">
       <p>{sentence}</p>
+
+      <input
+        className="text-black"
+        type="text"
+        value={written}
+        onChange={inputHandler}
+      />
     </div>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div className="bg-purple-700 h-[100vh]">
       <header>
-        <h1 className="font-mono text-xl pt-10 font-bold text-center">
+        <h1 className="font-mono text-2xl pt-10 font-extrabold text-center">
           typy _/
         </h1>
       </header>
